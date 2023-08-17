d594c8dcb60c31f364bedaf275bc25fc8c9eaa67
anatoliyivanov <itckem@ya.ru>
2023-08-17 03:47:35 +0300

    HelloWorldAnatoliy

---
+++ src/test/java/HelloWorldAnatoliy.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldAnatoliy {
+    @Test
+    public void testHelloWorld(){
+        System.out.println("Hello World Anatoliy");
+    }
+
+
+}
